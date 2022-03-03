0691454db425ee65b4f28f38c33fd94668f58958
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2022-03-03 13:53:13 +0530

    🎉 init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="fixed" color="primary">
+        <Toolbar>
+          <Typography variant="h6"></Typography>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
